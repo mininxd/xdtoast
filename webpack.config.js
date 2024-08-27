48ba3d1837e626cde882254848d6e270436de564
mininxd <mininsna@gmail.com>
2024-08-27 19:28:42 +0000

    rename toast.js to xdtoast.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path'); // Import the 'path' module
 module.exports = {
   entry: path.resolve('src/toast.js'),  // Ensure the entry path is absolute
   output: {
-    filename: 'toast.js',
+    filename: 'xdtoast.js',
     path: path.resolve('dist'),  // Convert the output path to an absolute path
     libraryTarget: 'module'
   },
